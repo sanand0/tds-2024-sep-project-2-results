ef0f4a1a88bafc14aa5efb19779aaeefbf49ca2d
S Anand <root.node@gmail.com>
2024-12-26 16:49:47 +0800

    ENH: Similarity table

--- similar.js
+++ similar.js
@@ -7,8 +7,29 @@ const codeLinks = Object.fromEntries(
   submissions.map((s) => {
     const id = s[submissions.columns[1]].split("@")[0];
     const link = s[submissions.columns[2]] ?? s[submissions.columns[2]];
-    return [id, link];
+    const time = new Date(s[submissions.columns[0]]);
+    const { owner, repo, branch } = parseGithubURL(link);
+    const githubURL = `https://github.com/${owner}/${repo}/tree/${branch}`;
+    return [id, { link, time, githubURL }];
   })
+);
+
+function parseGithubURL(url) {
+  if (!url) return null;
+  const parts = url.split("/");
+  if (parts.length < 7) return {};
+
+  // Return object with owner, repo, and branch properties
+  return parts[5] === "refs" && parts[6] === "heads"
+    ? { owner: parts[3], repo: parts[4], branch: parts[7] }
+    : { owner: parts[3], repo: parts[4], branch: parts[5] };
+}
+
+const results = await d3.csv("results.csv", d3.autoType);
+const marks = d3.rollup(
+  results.filter((d) => d.test != "diversity"),
+  (v) => d3.sum(v, (d) => d.marks),
+  (d) => d.id
 );
 
 const data = await d3.csv("pairwise.csv", d3.autoType);
@@ -28,16 +49,19 @@ function updateHash(value) {
   window.location.replace(`#?similarity=${value}`);
 }
 
+const pc = d3.format(".0%");
+
 function draw() {
   // Filter the data
   const minJaccard = +document.querySelector("#link-filter").value;
   updateHash(minJaccard);
   const filteredLinks = links.filter((d) => d.jaccard >= minJaccard);
-  nodes.forEach((d) => (d.n = d.links = 0));
+  nodes.forEach((d) => (d.n = d.links = d.maxJaccard = 0));
   for (const link of filteredLinks) {
-    nodeMap[link.target.value].n++;
+    nodeMap[link.source.value].n++;
     nodeMap[link.source.value].links++;
     nodeMap[link.target.value].links++;
+    nodeMap[link.source.value].maxJaccard = Math.max(nodeMap[link.source.value].maxJaccard, link.jaccard);
   }
 
   // Create the network
@@ -45,27 +69,44 @@ function draw() {
   const graph = network("#network", { nodes, links: filteredLinks, forces, brush, d3 });
 
   graph.nodes
-    .attr("fill", (d) => `var(--bs-${color(d)})`)
-    .attr("r", 5)
+    .attr("fill", (d) => `var(--bs-${nodeColor(d)})`)
+    .attr("r", (d) => 1 + (marks.get(d.value) ?? 0) / 2)
     .attr("data-bs-toggle", "tooltip")
     .attr("title", (d) => d.value);
-  graph.links.attr("stroke", "rgba(0,0,0,0.2)");
+  graph.links.attr("stroke", (d) =>
+    d.jaccard == 1 ? "rgba(255,0,0,0.2)" : d.jaccard > 0.6 ? "rgba(0,0,0,0.2)" : "rgba(0,0,0,0.05)"
+  );
 }
 
 function brush(nodes) {
-  const list = d3
+  if (!nodes.length) {
+    document.querySelector("#details").innerHTML = "Brush the network to select submissions and see details.";
+    return;
+  }
+  const rows = d3
     .sort(nodes, (a, b) => !!b.links - !!a.links || a.n - b.n)
     .map(
-      (d) => /* html */ `
-        <li><span class="text-${color(d)}">⬤</span> <a href="${codeLinks[d.value]}" target="_blank">
-          ${d.value}
-        </a></li>`
+      (d, i) => /* html */ `
+        <tr>
+          <td>${i + 1}</td>
+          <td class="text-${nodeColor(d)}">⬤</td>
+          <td>
+            <a href="${codeLinks[d.value]?.link}" target="_blank">${d.value}</a>
+            <a href="${codeLinks[d.value]?.githubURL}" target="_blank"><i class="bi bi-github"></i></a>
+          </td>
+          <td class="text-end">${(marks.get(d.value) ?? 0).toFixed(2)}</td>
+          <td>${codeLinks[d.value]?.time?.toLocaleString()}</td>
+          <td class="text-end">${pc(d.maxJaccard)}</td>
+        </tr>`
     )
     .join("");
-  document.querySelector("#details").innerHTML = `<ol>${list}</ol>`;
+  document.querySelector("#details").innerHTML = /* html */ `<table class="table table-sm table-striped sortable">
+    <thead><tr><th></th><th></th><th>ID</th><th>Marks</th><th>Time</th><th>% Sim</th></tr></thead>
+    <tbody>${rows}</tbody>
+  </table>`;
 }
 
-const color = (d) => (d.links == 0 ? "secondary" : d.n == 0 ? "success" : d.n == 1 ? "warning" : "danger");
+const nodeColor = (d) => (d.links == 0 ? "secondary" : d.n == 0 ? "success" : d.n == 1 ? "warning" : "danger");
 
 // Initialize from hash or default value
 function drawFromURL() {
